c8227bf5e03182ba51aff9c8255d358e8f1f1c72
hu1149173393 <72081260+hu1149173393@users.noreply.github.com>
2021-02-03 02:23:46 +0800

    增加一个导航栏部分

---
+++ src/components/navitionBar.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="bar">
+  <div v-for="(item,index) in array" :key="index">
+    <p>{{item.name}}</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name: "navitionBar",
+  data(){
+  return {
+    array:[
+      {
+        name:'个人中心'
+      },{
+        name:'时间记录'
+      },{
+        name:'分享经验'
+      },{
+        name: "一起嗨皮",
+      }
+    ]
+  }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.bar{
+  width: 100%;
+  background: black;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
